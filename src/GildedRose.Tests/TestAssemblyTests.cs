db571e3f3fc22bf526d6a3d01c20e443b20a90d5
RickyGian <rickygiankwan@gmail.com>
2026-04-22 21:27:06 +1000

    Create tests for basic items

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,13 +1,74 @@
 using Xunit;
+using GildedRose.Console;
+using System.Linq;
 
 namespace GildedRose.Tests
 {
-    public class TestAssemblyTests
+
+    public class GildedRoseTests
     {
-        [Fact]
-        public void TestTheTruth()
+        // Helper function to generate a Program with given items
+        private Program CreateProgramGivenItems(params Item[] items)
         {
-            Assert.True(true);
+            return new Program()
+            {
+                Items = items.ToList()
+            };
         }
+
+        #region Basic Items
+
+        [Fact]
+        public void BasicItem_DecreasesQualityAndSellInByOne()
+        {
+            var item = new Item { Name = "Basic Item", SellIn = 5, Quality = 10 };
+
+            var program = CreateProgramGivenItems(item);
+            program.UpdateQuality();
+
+            Assert.Equal(9, program.Items[0].Quality);
+            Assert.Equal(4, program.Items[0].SellIn);
+        }
+
+        [Fact]
+        public void BasicItem_QualityDegradesTwiceAfterSellInDate()
+        {
+            var item = new Item { Name = "Basic Item", SellIn = 0, Quality = 10 };
+
+            var program = CreateProgramGivenItems(item);
+            program.UpdateQuality();
+
+            Assert.Equal(8, program.Items[0].Quality);
+        }
+
+        [Fact]
+        public void BasicItem_QualityNeverNegative()
+        {
+            var item = new Item { Name = "Basic Item", SellIn = 10, Quality = 0 };
+            var program = CreateProgramGivenItems(item);
+
+            program.UpdateQuality();
+            Assert.Equal(0, program.Items[0].Quality);
+
+            program.UpdateQuality();
+            Assert.Equal(0, program.Items[0].Quality);
+        }
+
+
+        // Current UpdateQuality() implementation allows negative SellIn values
+        [Fact]
+        public void BasicItem_SellInCanGoNegative()
+        {
+            var item = new Item { Name = "Basic Item", SellIn = 0, Quality = 10 };
+            var program = CreateProgramGivenItems(item);
+
+            program.UpdateQuality();
+            Assert.Equal(-1, program.Items[0].SellIn);
+
+            program.UpdateQuality();
+            Assert.Equal(-2, program.Items[0].SellIn);
+        }
+
+        #endregion
     }
-}
+}   
